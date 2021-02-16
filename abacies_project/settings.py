499fc3a6062fc4006459b267ac62d2ddf12a95ea
Joel Hanson <joelhanson025@gmail.com>
2021-02-16 10:29:31 +0530

    Pagination for the apis added

--- abacies_project/settings.py
+++ abacies_project/settings.py
@@ -102,6 +102,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
